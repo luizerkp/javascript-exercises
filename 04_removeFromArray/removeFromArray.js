df795a28d376a79a53d7091805301a6e999e1362
Luis Tamarez <luistamarez@hotmail.com>
2022-02-21 15:56:04 -0800

    complete javascript-exercises/04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...removeElements) {
+    const filteredArr = arr.filter(function(element) {
+        return !removeElements.includes(element);
+    });
+    return filteredArr;
 };
 
 // Do not edit below this line
