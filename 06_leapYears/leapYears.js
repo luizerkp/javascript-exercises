d734eb6099ccbe8e40f860c3db97d6ab16788cde
Luis Tamarez <luistamarez@hotmail.com>
2022-02-21 15:58:03 -0800

    complete javascript-exercises/06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let leap = false;
+    const firstcheck = 4;
+    const secondcheck = 100;
+    const thirdcheck = 400;
+
+    if ((year % firstcheck) === 0 && (year % secondcheck) !== 0) {
+        leap = true;
+    }
+    if ((year % secondcheck) === 0 && (year % thirdcheck) === 0) {
+        leap = true;
+    }
+    return leap;   
 
 };
 
