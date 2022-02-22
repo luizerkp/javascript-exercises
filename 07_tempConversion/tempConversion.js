355cf7123677762cdcd87734ab32c3f0815e5f31
Luis Tamarez <luistamarez@hotmail.com>
2022-02-21 17:27:44 -0800

    complete javascript-exercises/07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+return roundToOneDecimal((fahrenheit - 32) * (5 / 9));
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  return roundToOneDecimal((celcius * 9 / 5)+ 32);
 };
+
+
+function roundToOneDecimal(num) {
+  return Math.round((num + Number.EPSILON) * 10) / 10;
+
+}
 
 // Do not edit below this line
 module.exports = {
