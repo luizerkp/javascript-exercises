cd0bdd99d04dd83551dab6c68fe24163f1ad9734
Luis Tamarez <luistamarez@hotmail.com>
2022-03-07 16:25:55 -0800

    -Finish calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+  arr.forEach(function(element) {total += element;});
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  arr.forEach(function(element) {total *= element;});
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
