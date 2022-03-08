6b41852fff6436cad8b480e6350a9cca7e75017a
Luis Tamarez <luistamarez@hotmail.com>
2022-03-07 18:35:45 -0800

    Delete accidentally created file 'md' -change variable 'sen' to starting -change variable 'reverse_sen' to 'reverse_string' -remove palindromes function dafult parameter

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,8 @@
-const palindromes = function (sen='ZZZZ car, a man, a maracaz.') {
-   sen = sen.toLowerCase().replace(/[^a-z]/g, '');
-   reverse_sen = sen.split('')
-   reverse_sen = reverse_sen.reverse().join('');
-   return sen === reverse_sen;
+const palindromes = function (string) {
+   string = string.toLowerCase().replace(/[^a-z]/g, '');
+   reverse_string = string.split('')
+   reverse_string = reverse_string.reverse().join('');
+   return string === reverse_string;
 };
 
 // Do not edit below this line
