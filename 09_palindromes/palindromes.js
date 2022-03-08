20e61d55f6536533001f97816b7ed269588a146c
Luis Tamarez <luistamarez@hotmail.com>
2022-03-07 18:29:06 -0800

    Complete palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (sen='ZZZZ car, a man, a maracaz.') {
+   sen = sen.toLowerCase().replace(/[^a-z]/g, '');
+   reverse_sen = sen.split('')
+   reverse_sen = reverse_sen.reverse().join('');
+   return sen === reverse_sen;
 };
 
 // Do not edit below this line
