25a4c976a0038fcd1af796ac4c949b0f286630e9
Luis Tamarez <luistamarez@hotmail.com>
2022-03-07 20:34:14 -0800

    Complete findTheOldest.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = {};
+    people.forEach(function(person) {
+        if (person.yearOfDeath === undefined) {
+            person.age = new Date().getFullYear() - person.yearOfBirth;
+        }
+        else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }        
+        if (oldest.age === undefined || person.age > oldest.age) {
+            oldest = person;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
