79eef50388607c6f7cafd0906221a1d45df5c38f
Luis Tamarez <luistamarez@hotmail.com>
2022-03-07 19:44:08 -0800

    Complete fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) {return "OOPS";}
 
+    let fib_array = [0, 1];
+    
+    for (let i = 2; i <= n; i++) {
+        fib_array.push(fib_array[i-1] + fib_array[i-2]);
+    }
+    return fib_array[n];
 };
 
 // Do not edit below this line
